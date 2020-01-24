e09c1c1ed61e585ca305a779a9eed79f3f68b885
MinexAutomation <minex.automation@gmail.com>
2020-01-23 16:46:48 -0800

    Added IVisualStudioSolutionFileSerializer service definition interface type.

---
+++ source/R5T.Solutas.Base/Code/Services/Definitions/IVisualStudioSolutionFileSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+using R5T.Megara;
+
+
+namespace R5T.Solutas
+{
+    public interface IVisualStudioSolutionFileSerializer : IFileSerializer<SolutionFile>
+    {
+    }
+}
